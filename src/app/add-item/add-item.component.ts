fcabfae7a4f3acac134aa440ab3a3c7d416976c6
Adam <b406434710@126.com>
2020-04-26 22:12:20 +0800

    add item form

--- src/app/add-item/add-item.component.ts
+++ src/app/add-item/add-item.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { finalize } from 'rxjs/operators';
+
+import { environment } from '@env/environment';
+import { Logger, untilDestroyed } from '@core';
 
 @Component({
   selector: 'app-add-item',
@@ -6,7 +12,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-item.component.scss'],
 })
 export class AddItemComponent implements OnInit {
-  constructor() {}
+  version: string | null = environment.version;
+  error: string | undefined;
+  addItemForm!: FormGroup;
+  isLoading = false;
+  constructor(private router: Router, private route: ActivatedRoute, private formBuilder: FormBuilder) {
+    this.createForm();
+  }
+  save() {
+    this.isLoading = true;
+  }
 
   ngOnInit(): void {}
+
+  private createForm() {
+    this.addItemForm = this.formBuilder.group({
+      price: ['', Validators.required],
+      stock: ['', Validators.required],
+      itemName: ['', Validators.required],
+    });
+  }
 }
--- src/app/add-item/add-item.module.ts
+++ src/app/add-item/add-item.module.ts
@@ -3,9 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { AddItemRoutingModule } from './add-item-routing.module';
 import { AddItemComponent } from './add-item.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { TranslateModule } from '@ngx-translate/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { I18nModule } from '@app/i18n';
 
 @NgModule({
   declarations: [AddItemComponent],
-  imports: [CommonModule, AddItemRoutingModule],
+  imports: [CommonModule, ReactiveFormsModule, TranslateModule, NgbModule, I18nModule, AddItemRoutingModule],
 })
 export class AddItemModule {}
