87db079ab0f83b499eb436687e8252106fa4c825
Adam <b406434710@126.com>
2020-05-27 00:21:15 +0800

    sign up

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -5,6 +5,7 @@ import { finalize } from 'rxjs/operators';
 
 import { environment } from '@env/environment';
 import { Logger, untilDestroyed } from '@core';
+import { HttpClient } from '@angular/common/http';
 const log = new Logger('SignUp');
 
 @Component({
@@ -18,7 +19,12 @@ export class SignUpComponent implements OnInit, OnDestroy {
   signUpForm!: FormGroup;
   isLoading = false;
 
-  constructor(private router: Router, private route: ActivatedRoute, private formBuilder: FormBuilder) {
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private formBuilder: FormBuilder,
+    private httpClient: HttpClient
+  ) {
     this.createForm();
   }
 
@@ -28,6 +34,29 @@ export class SignUpComponent implements OnInit, OnDestroy {
 
   signUp() {
     this.isLoading = true;
+
+    this.httpClient
+      .post('/signUp', {
+        username: this.signUpForm.value.username,
+        password: this.signUpForm.value.password,
+        type: this.signUpForm.value.role,
+      })
+      .pipe(
+        finalize(() => {
+          this.signUpForm.markAsPristine();
+          this.isLoading = false;
+        }),
+        untilDestroyed(this)
+      )
+      .subscribe(
+        (res) => {
+          this.router.navigate([this.route.snapshot.queryParams.redirect || '/'], { replaceUrl: true });
+        },
+        (error) => {
+          log.debug(`Login error: ${error}`);
+          this.error = error;
+        }
+      );
   }
 
   private createForm() {
